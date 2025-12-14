d42d35cff25761b12c2bc6b27fb97ff671826e2d
Michael N. <54519368+mnn7135@users.noreply.github.com>
2025-12-13 20:16:29 -0500

    PW-19 Update Dependecies (NextJS) (#20)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,19 @@
+import next from "eslint-config-next";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.config({
-    extends: ['next'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      'react/display-name': 'off'
-    },
-  }),
-];
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...next, ...compat.config({
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/display-name': 'off'
+  }
+}), {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
