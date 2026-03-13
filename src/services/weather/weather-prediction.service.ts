84ea0306fd0f7325ad4a7200a09afe1beca4d461
Michael N. <mnn7135@g.rit.edu>
2026-03-12 22:04:07 -0400

    PW-#24 Tweak Prediction System

--- src/services/weather/weather-prediction.service.ts
+++ src/services/weather/weather-prediction.service.ts
@@ -74,8 +74,7 @@ export default class IWeatherPredictionService {
             'windspdmph_avg10m'
         );
 
-        const predictedWindSpeed =
-            Math.abs(windTrendOverall) + Math.abs((pressureTrend * weatherData.length) / 3);
+        const predictedWindSpeed = Math.abs(windTrendOverall) + Math.abs(pressureTrend);
 
         const averageWindDirection = this.analysisService.getDataAverage(
             weatherData,
@@ -87,27 +86,17 @@ export default class IWeatherPredictionService {
             this.analysisService
                 .getHelperService()
                 .getWindDirection(averageWindDirection)
-                .includes('N')
+                .startsWith('N')
         ) {
             windDirectionEffect = -1; // Cold, Dry air from the north.
         }
         const predictedWindTemperatureDifference = predictedWindSpeed * 0.6 * windDirectionEffect;
-
-        /**
-         * Determine the temperature using the average of the maximum temperature and average temperature
-         * as recorded in the weatherData object. This should of course, also factor in the general
-         * temperature trend to determine a value.
-         */
         const temperatureTrend = this.analysisService.getDataTrend(weatherData, 'tempf');
 
         const predictedTemperature =
             weatherData[this.MOST_RECENT_DATA_INDEX].tempf +
             predictedWindTemperatureDifference +
-            temperatureTrend +
-            (((Math.abs(temperatureTrend) + 1) / (temperatureTrend + 1)) *
-                (this.analysisService.getDataMax(weatherData, 'tempf') -
-                    this.analysisService.getDataMin(weatherData, 'tempf'))) /
-                weatherData.length;
+            temperatureTrend;
 
         // Extreme changes in pressure tend to indicate unstable weather, potentially storms.
         if (Math.abs(pressureTrend) > this.PRESSURE_GRADIENT) {
@@ -172,7 +161,6 @@ export default class IWeatherPredictionService {
      * @returns the forecasted condition and temperature for three days from now
      */
     public getThreeDayForecast(): IWeatherForecast {
-        console.log(this.currentWeatherData);
         // Last two week's data.
         return this.getWeatherForecast([
             this.hasDataBeenSubmittedToday
